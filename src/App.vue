d7c5aee49523185233840e6710432fcceca7b415
Ricky Su <ricky@ez2.us>
2017-03-29 10:48:24 +0800

    加入 facebook login 元件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <com-facebook-login></com-facebook-login>
     <div class="row">
       <com-menu class="col-xs-3"></com-menu>
       <router-view class="col-xs-9"></router-view>
@@ -11,11 +12,13 @@
 <style src="font-awesome/css/font-awesome.css"></style>
 
 <script>
-import comMenu from '@/components/_menu.vue'
+import comMenu from '@/components/_menu'
+import comFacebookLogin from '@/components/_facebookLogin'
 export default {
   name: 'app',
   components: {
-    comMenu
+    comMenu,
+    comFacebookLogin
   }
 }
 </script>
---
+++ src/components/_facebookLogin.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="jumbotron">
+    <h1>請點選登入 Facebook!</h1>
+    <p><button @click.prevent="facebookLogin" class="btn btn-primary btn-lg"><i class="fa fa-facebook"></i> 登入 Facebook</button></p>
+  </div>
+</template>
