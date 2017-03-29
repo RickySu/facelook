584ff9a6a3815b5b1c3142033d780948d03faae5
Ricky Su <ricky@ez2.us>
2017-03-29 11:10:22 +0800

    加入登入後的 menu

--- src/components/_menu.vue
+++ src/components/_menu.vue
@@ -1,10 +1,17 @@
 <template>
-  <menu></menu>
+  <div class="list-group">
+    <router-link class="list-group-item" to=""> {{username}}</router-link>
+  </div>
 </template>
 
-<style>
-    menu {
-        height: 200px;
-        background: red;
+<script>
+export default{
+  computed: {
+    username: function () {
+      return this.$root.me.name
     }
+  }
+}
+</script>
+<style>
 </style>
