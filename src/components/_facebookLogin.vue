cffc18a378d793b9739f6c33c58b59794c5cae8d
Ricky Su <ricky@ez2.us>
2017-03-29 10:55:05 +0800

    初始化 hellojs

--- src/components/_facebookLogin.vue
+++ src/components/_facebookLogin.vue
@@ -4,3 +4,14 @@
     <p><button @click.prevent="facebookLogin" class="btn btn-primary btn-lg"><i class="fa fa-facebook"></i> 登入 Facebook</button></p>
   </div>
 </template>
+
+<script>
+import hello from 'hellojs/dist/hello.all.js'
+export default {
+  created: function(){
+    hello.init({
+      facebook: '1314748098607534'
+    })
+  }
+}
+</script>
