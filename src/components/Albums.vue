2a5637ca1b7c75b6d2aa390f2a1a09b0ed5c3c86
Ricky Su <ricky@ez2.us>
2017-04-05 13:47:09 +0800

    發送 api 取得相簿內容

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,1 +1,23 @@
 <template></template>
+
+<script>
+export default{
+  data: () => {
+    return {
+      albums: []
+    }
+  },
+  created: function () {
+    var self = this
+    this.$http.get('https://graph.facebook.com/v2.8/' + this.$root.me.id + '/albums', {
+      params: {
+        access_token: this.$root.authResponse.access_token,
+        fields: 'name,cover_photo,count'
+      }
+    })
+    .then((result) => {
+      me.albums = result.body.data
+    })
+  }
+}
+</script>
