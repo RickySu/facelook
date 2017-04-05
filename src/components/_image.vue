db82067405701add728c2ec0ce01e561af9434a6
Ricky Su <ricky@ez2.us>
2017-04-05 14:00:32 +0800

    重構 imgSrc 屬性

--- src/components/_image.vue
+++ src/components/_image.vue
@@ -1,9 +1,14 @@
 <template>
-  <img :src="'https://graph.facebook.com/v2.8/' + id + '/picture?access_token=' + $root.authResponse.access_token" />
+  <img :src="imgSrc"/>
 </template>
 
 <script>
-  export default{
-    props: ['id']
+export default{
+  props: ['id'],
+  computed: {
+    imgSrc: function () {
+      return 'https://graph.facebook.com/v2.8/' + this.id + '/picture?access_token=' + this.$root.authResponse.access_token
+    }
   }
+}
 </script>
