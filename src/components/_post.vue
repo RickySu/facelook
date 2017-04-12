684d3e9841848636c708e6b13eca242e43dacfea
Ricky Su <ricky@ez2.us>
2017-04-12 10:49:53 +0800

    綁定 v-model message

--- src/components/_post.vue
+++ src/components/_post.vue
@@ -2,7 +2,7 @@
   <form class="form-horizontal">
     <div class="form-group">
       <label class="control-label">建立貼文</label>
-      <textarea class="form-control" rows="3"></textarea>
+      <textarea class="form-control" rows="3" v-model="message"></textarea>
     </div>
     <div class="form-group">
       <div class="pull-right">
@@ -14,9 +14,14 @@
 
 <script>
 export default{
+  data: () => {
+    return {
+      message: ''
+    }
+  },
   methods: {
     send: function(){
-      
+      console.log(this.message)
     }
   }
 }
